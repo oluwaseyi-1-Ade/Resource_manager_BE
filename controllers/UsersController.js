36bd103c2334789061db2e439e6f89e44e17680e
oluwaseyi-1-Ade <olatubosunoluwaseyi1@gmail.com>
2026-09-16 00:01:59 +0100

    fetch users

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -12,14 +12,32 @@ export const getAllUsers = async (req, res) => {
         .json({ message: "You are forbidden to carry out this operation" });
     }
 
-    const queryFilters = req.query;
+    // Extract page and limit (with defaults), put the rest into queryFilters
+    const { page = 1, limit = 10, ...queryFilters } = req.query;
 
-    const allUsers = await Users.findAll({
+    // Convert them to numbers (URL queries are always strings by default)
+    const pageInt = parseInt(page);
+    const limitInt = parseInt(limit);
+
+    //  calculate ofset- number of items to skip
+    const offset = (pageInt - 1) * limitInt;
+
+    const { count, rows: allUsers } = await Users.findAndCountAll({
       where: queryFilters,
+      limit: limitInt,
+      offset: offset,
       attributes: { exclude: ["password"] },
     });
 
-    res.status(200).json({ message: "Success", data: allUsers });
+    res.status(200).json({
+      message: "Success",
+      data: allUsers,
+      pagination: {
+        totalItems: count,
+        totalPages: Math.ceil(count / limitInt),
+        currentPage: pageInt,
+      },
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
